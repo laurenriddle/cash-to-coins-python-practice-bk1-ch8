108aaaaea29715c7e790cae8c755c4cf6fa7cb32
Lauren Riddle <lari7132@gmail.com>
2020-01-14 13:47:13 -0600

    added comments

--- cashToCoins.py
+++ cashToCoins.py
@@ -1,3 +1,6 @@
+'''
+Now do the reverse. Convert the dollar amount into the coins that make up that dollar amount. The final result is an object with the correct number of quarters, dimes, nickels, and pennies.
+'''
 dollarAmount = 18.63
 
 piggyBank = {
@@ -9,24 +12,35 @@ piggyBank = {
 
 
 def cash_to_coins():
-    # dollars = int(dollarAmount)
-    # cents = dollarAmount - int(dollarAmount)
-    
-    # quarters = (dollars * 4) + cents % .25
+    # finds the total number of quarters that can be used for this dollar amount
     quarters = (int(dollarAmount) * 4) + int((dollarAmount - int(dollarAmount)) / .25)
+    # puts the number of quarters into the piggy bank
     piggyBank["quarters"] = quarters
 
+    # find the change left now in the dollar amount
     initial_change = dollarAmount - (quarters * .25)
 
+    # finds the total number of dimes that can be used 
     dimes = int(initial_change / .10)
+    # puts the number of dimes into the piggy bank
     piggyBank["dimes"] = dimes
+
+    # find the change left now in the dollar amount
     change_after_dimes = initial_change - dimes * .10
+    # converts the change to a whole number
     convert_change = round(change_after_dimes * 100)
-    nickles = int(convert_change / 5)
-    piggyBank["nickels"] = nickles
-    pennies = convert_change - nickles * 5
+
+    # finds the total number of nickels that can be used 
+    nickels = int(convert_change / 5)
+    # puts the number of nickels into the piggy bank
+    piggyBank["nickels"] = nickels
+
+    # finds the total number of pennies that can be used 
+    pennies = convert_change - nickels * 5
+    # puts the number of pennies into the piggy bank
     piggyBank["pennies"] = pennies
+
+    print(piggyBank)
 
 cash_to_coins()
 
-print(piggyBank)
